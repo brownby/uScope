111b9dde03cc1e90b93bb85723a789584b254f27
beb539 <brown@seas.harvard.edu>
2020-08-12 19:24:04 -0400

    Updated string descriptor construction logic in USB_Handler

--- uScope.ino
+++ uScope.ino
@@ -692,11 +692,13 @@ void USB_Handler(){
 
             memset(usb_string_descriptor_buffer, 0, sizeof(usb_string_descriptor_buffer));
 
-            usb_string_descriptor_buffer[0] = len*2 + 2;
+//            usb_string_descriptor_buffer[0] = len*2 + 2;
+            usb_string_descriptor_buffer[0] = len + 2; // changed from ataradov doing len*2 + 2, unclear why multiply by 2
             usb_string_descriptor_buffer[1] = USB_STRING_DESCRIPTOR;
 
             for (int i = 0; i < len; i++){
-              usb_string_descriptor_buffer[2 + i*2] = str[i];
+//              usb_string_descriptor_buffer[2 + i*2] = str[i];
+              usb_string_descriptor_buffer[2 + i] = str[i]; // also unclear here why ataradov was multiplying by 2 on the index
             }
 
             leng = LIMIT(leng, string0Descriptor_usb.bLength);
