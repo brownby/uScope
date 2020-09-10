307c890e9815342cf96ac07e8faaebc6c12ee8c0
beb539 <brown@seas.harvard.edu>
2020-09-10 12:21:32 -0400

    Somehow massively reduced glitches

--- src/usb_descriptors.cpp
+++ src/usb_descriptors.cpp
@@ -134,8 +134,8 @@ alignas(4) usb_configuration_hierarchy_t usb_configuration_hierarchy = {
     .bDescriptorSubtype  = 0x01,   // general
     .bTerminalLink       = 0x02,   // ID for output scope terminal
     .bDelay              = 0x00,   // 0x01 in example, interface delay *flag
-    //.wFormatTag          = 0x0002, // 0x0002 for Windows, 0x0001 for Mac
-    .wFormatTag          = 0x0001, // 0x0001 = PCM, 0x0002 = PCM8 *flag 
+    .wFormatTag          = 0x0002, // 0x0002 for Windows, 0x0001 for Mac
+    // .wFormatTag          = 0x0001, // 0x0001 = PCM, 0x0002 = PCM8 *flag 
                                    // macOS recognizes PCM, but not PCM8 (legacy = unsupported)? 
   },
 
@@ -146,7 +146,9 @@ alignas(4) usb_configuration_hierarchy_t usb_configuration_hierarchy = {
     .bDescriptorSubtype  = 0x02, // format
     .bFormatType         = 0x01, // type I
     .bNrChannels         = 0x01, // 1 channel
-    .bSubframeSize       = 0x02, // 1 byte per audio subframe 
+    // .bSubframeSize       = 0x02, // 2 bytes per audio subframe 
+    .bSubframeSize       = 0x01,
+    // .bBitResolution      = 0x10, // 16 bits
     .bBitResolution      = 0x08, // 8 bits 
     .bSamFreqType        = 0x01, // 1 sampling frequency
     .bSamFreq0_byte0     = 0x44,
